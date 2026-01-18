efcdd64139b4d3ab1ff24a79602a3801bcd12a4f
Swastika Gupta <swas.manigupta@gmail.com>
2026-01-18 17:23:52 +0530

    Add loop and exit option to calculator

--- calculator.py
+++ calculator.py
@@ -1,33 +1,45 @@
 print("Simple Calculator")
 
-num1 = float(input("Enter First Number:"))
-num2 = float(input("Enter Second Number:"))
+while True:
+    
+    num1 = float(input("Enter First Number:"))
+    num2 = float(input("Enter Second Number:"))
 
-print("Choose Operation:")
-print("1. Addition ")
-print("1. Subtraction ")
-print("1. Multiplication ")
-print("1. Division ")
 
-choice = input ("Enter your choice(1/2/3/4):")
+    print("Choose Operation:")
+    print("1. Addition ")
+    print("1. Subtraction ")
+    print("1. Multiplication ")
+    print("1. Division ")
+    print("5. Exit.")
 
-if choice == '1':
-    print("Result:", num1 + num2 )
+    choice = input ("Enter your choice(1/2/3/4/5):")
 
-elif choice == '2':
-    print("Result:", num1 - num2 )
+    if choice == '1':
+        print("Result:", num1 + num2 )
 
-elif choice == '3':
-    print("Result:", num1 *  num2 )
+    elif choice == '2':
+        print("Result:", num1 - num2 )
 
-elif choice == '4':
-    if num2 != 0 : 
-        print("Result:", num1 / num2 )
+    elif choice == '3':
+        print("Result:", num1 *  num2 )
+
+    elif choice == '4':
+        if num2 != 0 : 
+            print("Result:", num1 / num2 )
+        else :
+            print ("Error:Division by Zero")
+
+    elif choice == '5':
+        print("Exiting Calculator. Goodbye!")
+        break
+
     else :
-        print ("Error:Division by Zero")
-else :
-    print("Invalid Choice!")
+        print("Invalid Choice!")
 
-input("Press Enter to exit.")
+    print()
+
+
+
 
 
